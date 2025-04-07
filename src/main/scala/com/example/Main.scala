0e463d343cbf1b7e9d5dda42a3df7194b738e763
fhhmm <takahashi.t66@gmail.com>
2025-04-07 22:01:12 +0900

    account情報も出力

--- src/main/scala/com/example/Main.scala
+++ src/main/scala/com/example/Main.scala
@@ -11,10 +11,10 @@ object Main extends App {
   val accountService = injector.getInstance(classOf[AccountService])
 
   println("=== User Information ===")
-  println(s"User 1: ${userService.getUserBy("1")}")
-  println(s"User 2: ${userService.getUserBy("2")}")
-  println(s"User 3: ${userService.getUserBy("3")}")
+  println(s"User 1: ${userService.getUserBy("1")}, Account: ${accountService.getAccountBy(1L)}")
+  println(s"User 2: ${userService.getUserBy("2")}, Account: ${accountService.getAccountBy(2L)}")
+  println(s"User 3: ${userService.getUserBy("3")}, Account: ${accountService.getAccountBy(3L)}")
   println(
-    s"Unknown User: ${userService.getUserBy("999")}"
+    s"Unknown User: ${userService.getUserBy("999")}, Account: ${accountService.getAccountBy(999L)}"
   )
 }
